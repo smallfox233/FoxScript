97bdd1cd1d22708531886699c9a9d0293d868206
小狐狸FM <51869871+smallfox233@users.noreply.github.com>
2022-07-05 11:06:13 +0800

    过滤nmap扫描结果，正则匹配获取开放端口

--- nmap_get_port.py
+++ nmap_get_port.py
@@ -1,9 +1,11 @@
 # 作者：小狐狸FM
-# 功能：过滤nmap扫描结果，获取开放端口
+# 功能：过滤nmap扫描结果，正则匹配获取开放端口
+import re
 file_pth = "data/nmap.txt" #读取的文件路径
 result_pth = "data/result.txt" #写入的文件路径
 nmap_lis = []
 result_lis = []
+pattern = "\d{1,6}/" #正则表达式，端口/协议
 #读取
 with open(file_pth,"r") as fp:
     nmap_lis = list(fp.read().split("\n"))
@@ -11,7 +13,7 @@ with open(file_pth,"r") as fp:
 #显示
 for i in nmap_lis:
     if "open" in i:
-        result_lis.append(i.split("/")[0])
+        result_lis.append(re.findall(pattern,i)[0].replace("/",""))
 #写入
 with open(result_pth,"w") as fp:
     fp.write("\n".join(result_lis))
