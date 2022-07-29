2fedfca72dc7c6a16ff5ca217f82bfda7a6b2381
小狐狸FM <51869871+smallfox233@users.noreply.github.com>
2022-07-29 10:54:52 +0800

    Update http_scan.py

--- http_scan.py
+++ http_scan.py
@@ -1,35 +1,41 @@
 # coding=utf-8
 # 作者：小狐狸FM
-# 检测状态码
+# GET请求，检测状态码
 import requests
 
-#文本路径
+# 文本路径
 pth = "url.txt"
 new_pth = "new.txt"
-#连接超时时长,秒
+# 连接超时时长,秒
 time = 3
-#匹配的状态码，符合时才会将url写入文件
-state = 200
-#需要添加到url末尾的路径
-add = ""
-#字符串
+# 匹配的状态码，符合时才会将url写入文件
+state = ['200','302','403']
+# 需要添加到url末尾的路径，可用于IP不同，路径相同的情况
+add_right = ""
+# 需要添加到url开头的路径，可用于IP相同，路径不同的情况
+add_left = "https://61.154.9.144"
+# 字符串
 s1 = ""
 s2 = ""
-#文件读取
-with open(pth,"r") as fp:
+# 文件读取
+with open(pth, "r") as fp:
     s1 = fp.read()
 lis = list(s1.split("\n"))
-#遍历
+# print(lis)
+# 遍历
 for url in lis:
+    now_url = add_left + url + add_right
     try:
-        r = requests.get(url,timeout=time)
-        if r.status_code == state:
-            s2 += url + add + "\n"
-            print(url)
+        r = requests.get(now_url, timeout=time)
+        if str(r.status_code) in state:
+            s2 += now_url + "\n"
+            print("成功：",now_url)
+        else:
+            print("失败：",now_url)
     except:
+        print("失败：",now_url)
         continue
-#文件写入
-with open(new_pth,"w") as fp:
+# 文件写入
+with open(new_pth, "w") as fp:
     fp.write(s2)
-
-
+print("结果输出至文件: ",new_pth)
