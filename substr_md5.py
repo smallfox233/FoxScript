ba52dd64c4e171f9f20d424ae30826e2f82390b3
小狐狸FM <51869871+smallfox233@users.noreply.github.com>
2022-10-18 07:48:28 +0800

    Update substr_md5.py

--- substr_md5.py
+++ substr_md5.py
@@ -17,7 +17,7 @@ char = string.ascii_letters + string.digits
 # 需要匹配的子串内容(必须)
 sub_str = "8b184b"
 # 明文长度(必须)
-m_len = 8
+m_len = 10
 # 已爆破过的明文
 m_lis = []
 # 已爆破过的明文存放路径
@@ -28,23 +28,23 @@ sub_start = 6
 sub_len = 6
 # 需要匹配的子串结束位置
 sub_end = sub_start + sub_len
-if sub_start >= m_len: #初始位置超过主串长度时
+if sub_start >= m_len:  # 初始位置超过主串长度时
     print("md5_len必须大于sub_start!!")
     sys.exit()
-if sub_end< 0 : #子串长度负数
+if sub_end < 0:  # 子串长度负数
     print("sub_end不能为负数!!")
     sys.exit()
-with open(m_pth,"a+") as fp:
+with open(m_pth, "a+") as fp:
     m_lis = fp.read().split("\n")
-    while True: #死循环
+    while True:  # 死循环
         # 明文
         m = ''.join(random.choice(char) for i in range(m_len))
         # 密文,32位md5
         c = hashlib.md5(m.encode())
-        if m not in m_lis and m[sub_start:sub_len:]==sub_str: #未生成过的明文，且成功获取内容时
+        if m not in m_lis and m[sub_start:sub_end:] == sub_str:  # 未生成过的明文，且成功获取内容时
             print("解密成功！")
-            print("明文:",m)
-            print("密文:",c)
+            print("明文:", m)
+            print("密文:", c)
             break
         else:
-            fp.write(m + "\n")
+            fp.write(m + " " + c + "\n")
