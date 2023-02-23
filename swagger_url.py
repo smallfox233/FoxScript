f58e1db4a329c4ca432c89368910d868f5f7d9e7
小狐狸FM <51869871+smallfox233@users.noreply.github.com>
2023-02-23 16:22:56 +0800

    提取获取swagger接口中的路径信息

---
+++ swagger_url.py
@@ -0,0 +1,17 @@
+# 作者：小狐狸FM
+# 项目：https://github.com/smallfox233/FoxScript
+# 功能：提取swagger v2/docs中的路径信息
+import json
+tmp = ""
+result = ""
+filename = "xxx.json" #目标文件
+with open(filename,"r",encoding='utf-8') as fp:
+    tmp = fp.read()
+# print(tmp)
+tmp = json.loads(tmp)
+# print(tmp)
+for key,value in tmp.items():
+    if key=='paths':
+        # print(tmp[key])
+        for url,j in value.items():
+            print(url)
